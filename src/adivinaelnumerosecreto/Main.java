53caeff71dc93a580cd9cafb94a487d8e2d7ff4e
Juan Manuel Chi Rejón <15470156@itcampeche.edu.mx>
2016-04-05 20:49:05 -0500

    Proyecto con sólo la implementación de la clase NumeroSecreto

---
+++ src/adivinaelnumerosecreto/Main.java
@@ -0,0 +1,22 @@
+/*
+Alumno: Chi Rejón Juan Manuel
+Semestre: Segundo
+Grupo: MS2
+Laboratorio 1.- El Número Secreto
+ */
+package adivinaelnumerosecreto;
+
+/**
+ *
+ * @author juanm
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/model/NumeroSecreto.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.util.Random;
+/**
+ *
+ * @author juanm
+ */
+public class NumeroSecreto {
+    private Integer numero;
+    private Integer limiteInferior;
+    private Integer limiteSuperior;
+    private Random generador;
+    
+    private static final int LIM_INF_PREDET = 1;
+    private static final int LIM_SUP_PREDET = 100;
+    
+    public void setRango(int limiteInferior, int limiteSuperior) {
+        if (limiteInferior > limiteSuperior) {
+            this.limiteInferior = LIM_INF_PREDET;
+            this.limiteSuperior = LIM_SUP_PREDET;
+        }
+        else {
+            this.limiteInferior = limiteInferior;
+            this.limiteSuperior = limiteSuperior;
+        }
+    }
+    
+    public final void generar() {
+            this.numero = generador.nextInt(limiteSuperior) + 1;
+        }
+    
+    public NumeroSecreto(int limiteInferior, int limiteSuperior){
+        this.setRango(limiteInferior, limiteSuperior);
+        this.generador = new Random();
+        this.generar();
+    }
+    
+    public NumeroSecreto () {
+        this(LIM_INF_PREDET, LIM_SUP_PREDET);
+    }
+    
+    public int getNumeroSecreto() {
+        return this.numero;
+    }
+    public int getLimiteInferior() {
+        return this.limiteInferior;
+    }
+    public int getLimiteSuperior() {
+        return this.limiteSuperior;
+    }
+    
+    public boolean esMayor(int numero) {
+        boolean elNumeroSecretoEsMayor;
+        if (this.numero > numero) {
+            elNumeroSecretoEsMayor = true;
+        }
+        else {
+            elNumeroSecretoEsMayor = false;
+        }
+        return elNumeroSecretoEsMayor;
+    }
+    
+    public boolean esMenor(int numero) {
+        boolean elNumeroSecretoEsMenor = false;
+        if (this.numero < numero) {
+            elNumeroSecretoEsMenor = true;
+        }
+        return elNumeroSecretoEsMenor;
+    }
+    
+    public boolean esIgual(int numero) {
+        return (this.numero == numero);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
