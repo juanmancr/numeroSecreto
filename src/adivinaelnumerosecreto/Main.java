e24f6e7d374255eae8f2953be9d985e8756c1f8f
Juan Manuel Chi Rejón <15470156@itcampeche.edu.mx>
2016-04-06 20:34:28 -0500

    Primera versión completa del proyecto

--- src/adivinaelnumerosecreto/Main.java
+++ src/adivinaelnumerosecreto/Main.java
@@ -5,6 +5,8 @@ Grupo: MS2
 Laboratorio 1.- El Número Secreto
  */
 package adivinaelnumerosecreto;
+import java.util.Scanner;
+import model.Juego;
 
 /**
  *
@@ -17,6 +19,26 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        boolean seAdivinoElNumeroSecreto = false;
+        Juego juego = new Juego(1,100,5);
+        while (juego.puedeIntentarDeNuevo() == true
+                && !seAdivinoElNumeroSecreto) {
+            System.out.print("Adivine el número secreto: ");
+            numero = entrada.nextInt();
+            seAdivinoElNumeroSecreto = juego.esElNumeroSecreto(numero);
+            if (seAdivinoElNumeroSecreto) {
+                System.out.println("Felicidades. Has adivinado el número secreto.");
+                }
+            else {
+                System.out.println("Fallaste. El número secreto es "
+                        + juego.getTextoDeAyuda(numero)
+                        + " que el número proporcionado");
+                System.out.println("Intenta de nuevo.");
+                }
+            }
+        System.out.println("El número secreto es " + juego.getNumeroSecreto());
+    }                    
+      
 }
